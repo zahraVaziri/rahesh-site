41feb8b0f75fb8660af735d0307217b48ad2b16a
Amirhossein <amir.khaligh@yahoo.com>
2021-10-14 23:00:41 +0330

    fix lines ellipsis responsive

--- src/components/pages/product/Product.js
+++ src/components/pages/product/Product.js
@@ -1,47 +1,47 @@
+import React from "react";
 import HTMLEllipsis from 'react-lines-ellipsis/lib/html';
 import LinesEllipsis from 'react-lines-ellipsis';
 import responsiveHOC from 'react-lines-ellipsis/lib/responsiveHOC'
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
-const ResponsiveEllipsis = responsiveHOC()(LinesEllipsis)
-const Product = (props) => {
-const data = props.data
+import {Link} from "react-router-dom";
+const LinesEllipsisRes = responsiveHOC()(LinesEllipsis);
+const HTMLEllipsisRes = responsiveHOC()(HTMLEllipsis);
 
-         
-    return(
+const Product = (props) => {
+    const data = props.data;
+
+    return (
         <>
-        {ResponsiveEllipsis} 
-           <div class="card">
-                <img src= {data.pic} class="card-img-top" alt="..." width={'100%'}/>
-                    <div class="card-body">
-                        <Link  to={'/detail/'+ data.id}>
-                        
-                            <LinesEllipsis
-                                text={data.title}
-                                maxLine='1'
-                                ellipsis='...'
-                                basedOn='letters'
-                                component='h4'
-                            />
-                                        
-                        </Link>
-                                <span class="cat-nb">پژوهش</span>
-                                <span class="date-nb">02 ژوئن 2021</span>
-                                <p class="line3-ell">
-                                    <HTMLEllipsis
-                                        unsafeHTML={data.text}
-                                        maxLine='3'
-                                        ellipsis='...'
-                                        basedOn='letters'
-                                    />
-                                </p>
-                            </div>
-                            </div> 
-                            
-            
+            <div className="card">
+                <img src={data.pic} className="card-img-top" alt="..." width={'100%'}/>
+                <div className="card-body">
+                    <Link to={'/detail/' + data.id}>
+
+                        <LinesEllipsisRes
+                            text={data.title}
+                            maxLine='1'
+                            ellipsis='...'
+                            basedOn='letters'
+                            component='h4'
+                        />
+
+                    </Link>
+                    <span className="cat-nb">پژوهش</span>
+                    <span className="date-nb">02 ژوئن 2021</span>
+                    <p className="line3-ell">
+                        <HTMLEllipsisRes
+                            unsafeHTML={data.text}
+                            maxLine='3'
+                            ellipsis='...'
+                            basedOn='letters'
+                        />
+                    </p>
+                </div>
+            </div>
+
+
         </>
     )
-    
+
 }
 
 export default Product
--- src/components/pages/product/ProductList.js
+++ src/components/pages/product/ProductList.js
@@ -1,8 +1,12 @@
 import Product from "./Product";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {SwiperSlide, Swiper}  from 'swiper/react';
 
 const ProductList = (props) => {
+    const [amir,setAmir]=useState(false)
+    useEffect(()=>{
+        setAmir(true)
+    })
     return(
         <>
          <Swiper slidesPerView={1} spaceBetween={5} autoplay={{
@@ -16,16 +20,18 @@ const ProductList = (props) => {
                         },
                         "768": {
                             "slidesPerView": 3,
-                            "spaceBetween": 40
+                            "spaceBetween": 30
                         },
                         "1024": {
                             "slidesPerView": 4,
-                            "spaceBetween": 50
+                            "spaceBetween": 40
                         }
                         }} className="mySwiper py-5">
             {props.products.map((item)=> (
                <SwiperSlide className='py-2'>
-                    <Product data={item} />
+                   {
+                       amir ? <Product data={item} /> : ''
+                   }
                </SwiperSlide>
                 )
                
